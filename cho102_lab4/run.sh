5b668153226ed156d47e3df1ace4e924212725fd
Cindy Ho <cho102@ucr.edu>
2023-02-03 21:57:11 +0000

    finished

--- cho102_lab4/run.sh
+++ cho102_lab4/run.sh
@@ -1,4 +1,3 @@
 mvn clean package
 yarn jar target/cho102_lab4-1.0-SNAPSHOT.jar edu.ucr.cs.cs167.cho102.Filter hdfs:///nasa_19950801.tsv hdfs:///filter_output.tsv 200
-mvn clean package
 yarn jar target/cho102_lab4-1.0-SNAPSHOT.jar edu.ucr.cs.cs167.cho102.Aggregation hdfs:///nasa_19950801.tsv hdfs:///aggregation_output.tsv
